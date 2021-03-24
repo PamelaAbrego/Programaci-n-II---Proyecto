896f5f3a1fd6d890f7effef34b1cc51ad8a8c823
Pamela Abrego <pamelabregor@gmail.com>
2021-03-23 20:44:39 -0600

    Contenido

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/principal")
+def principal():
+    return render_template("principal.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/gruas")
+def gruas():
+    return render_template("gruas.html")
+
+
+@app.route("/cotizaciones")
+def cotizaciones():
+    return render_template("cotizaciones.html")
 
 
 if __name__ == "__main__":
