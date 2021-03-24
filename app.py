ce8a195e3be182025d14548f831341830afc1b9e
Pamela Abrego <pamelabregor@gmail.com>
2021-03-23 22:01:57 -0600

    Páginas extra

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
-
 app = Flask(__name__)
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/principal")
-def principal():
+def home():
     return render_template("principal.html")
 
 
