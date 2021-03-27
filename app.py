d6b6807507c0865ec9e624f5d01c3c23dc1ccff3
Pamela Abrego <pamelabregor@gmail.com>
2021-03-27 16:30:02 -0600

    Enlaces con páginas

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/")
