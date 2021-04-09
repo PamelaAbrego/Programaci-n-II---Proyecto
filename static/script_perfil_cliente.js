3824a219394263b882c98c43924b2be1e163efb9
Francisco <alejandrofr15@gmail.com>
2021-04-08 23:49:05 -0600

    AgregarTablaCli

--- static/script_perfil_cliente.js
+++ static/script_perfil_cliente.js
@@ -1,7 +1,9 @@
 window.onload = cargar;
 
+
 function cargar() {
     datos();
+    cargarDatosTablaProy();
     cargarDatos();
 }
 function readArrayFromLocalStorage(keyName) {
@@ -38,18 +40,19 @@ function registrosRecibidos() {
     var recibidos = readArrayFromLocalStorage("RegistrosRecibidos");
     var myTable = document.getElementById("tabla");
     var numRows = myTable.getElementsByTagName('tr').length - 1
-
-    for (var i = 1; i <= numRows; i++) {
-        user = myTable.rows.item(i).cells.item(0).innerHTML
-        email = myTable.rows.item(i).cells.item(1).innerHTML
-        modelo = myTable.rows.item(i).cells.item(2).innerHTML
-        cantidad = myTable.rows.item(i).cells.item(3).innerHTML
-        ubicacion = myTable.rows.item(i).cells.item(4).innerHTML
-        fecha = myTable.rows.item(i).cells.item(5).innerHTML
-        tiempo = myTable.rows.item(i).cells.item(6).innerHTML
-        for (var j = 0; j < recibidos.length; j++) {
-            if (user === recibidos[j].user && email === recibidos[j].email && modelo === recibidos[j].modelo && cantidad === String(recibidos[j].cantidad) && ubicacion === recibidos[j].ubicacion && fecha === recibidos[j].fecha && tiempo === String(recibidos[j].tiempo)) {
-                myTable.rows.item(i).cells.item(7).innerHTML = "Recibido";
+    if (recibidos !== null) {
+        for (var i = 1; i <= numRows; i++) {
+            user = myTable.rows.item(i).cells.item(0).innerHTML
+            email = myTable.rows.item(i).cells.item(1).innerHTML
+            modelo = myTable.rows.item(i).cells.item(2).innerHTML
+            cantidad = myTable.rows.item(i).cells.item(3).innerHTML
+            ubicacion = myTable.rows.item(i).cells.item(4).innerHTML
+            fecha = myTable.rows.item(i).cells.item(5).innerHTML
+            tiempo = myTable.rows.item(i).cells.item(6).innerHTML
+            for (var j = 0; j < recibidos.length; j++) {
+                if (user === recibidos[j].user && email === recibidos[j].email && modelo === recibidos[j].modelo && cantidad === String(recibidos[j].cantidad) && ubicacion === recibidos[j].ubicacion && fecha === recibidos[j].fecha && tiempo === String(recibidos[j].tiempo)) {
+                    myTable.rows.item(i).cells.item(7).innerHTML = "Recibido";
+                }
             }
         }
     }
@@ -81,3 +84,30 @@ function cargarDatos() {
     }
     registrosRecibidos();
 }
+
+function cargarDatosTablaProy() {
+    var arrayProy = readArrayFromLocalStorage("lAddResultArrayPro");
+    var myTableProy = document.getElementById("tablaProy");
+    var numeroAlquileres = 0;
+    if (arrayProy !== null) {
+        for (var i = 0; i < arrayProy.length; i++) {
+            if (arrayProy[i].user === document.getElementById("user_cliente").innerHTML) {
+                var row = myTableProy.insertRow(1);
+                row.insertCell(0).innerHTML = arrayProy[i].fec3;
+                row.insertCell(1).innerHTML = arrayProy[i].tip;
+                row.insertCell(2).innerHTML = arrayProy[i].nom;
+                row.insertCell(3).innerHTML = arrayProy[i].cor;
+                row.insertCell(4).innerHTML = arrayProy[i].num;
+                row.insertCell(5).innerHTML = arrayProy[i].fec1;
+                row.insertCell(6).innerHTML = arrayProy[i].fec2;
+                row.insertCell(7).innerHTML = arrayProy[i].ubi;
+                row.insertCell(8).innerHTML = "Enviado";
+                numeroAlquileres++;
+            }
+        }
+    }
+    if (numeroAlquileres === 0) {
+        alert("No has realizado ninguna solicitud de cotización")
+    }
+}
+
