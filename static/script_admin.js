446c4d819b383733300251eb756880cf051c7501
Pamela Abrego <pamelabregor@gmail.com>
2021-04-19 00:36:58 -0600

    Aceptar y Rechazar Cotizaciones TERMINADO

--- static/script_admin.js
+++ static/script_admin.js
@@ -50,14 +50,12 @@ function actDatos() {
         row.insertCell(6).innerHTML = array[i].tiempo;
         row.insertCell(7).innerHTML = "<button onclick='aceptarAlquiler(" + i + ")'>Aceptar</button>";
         row.insertCell(8).innerHTML = "<button onclick='rechazarAlquiler(" + i + ")'>Rechazar</button>";
-        //row.insertCell(9).innerHTML = "<button onclick='eliminarAlquiler(" + i + ")'>Eliminar</button>";
         array[i].estado = "Recibido"
 
         addResultToStorageRegistrosRecibidos(array[i].user, array[i].email, array[i].modelo, array[i].cantidad, array[i].ubicacion, array[i].fecha, array[i].tiempo)
     }
     cargarAceptados();
     cargarRechazados();
-    //cargarEliminados();
 }
 
 function aceptarAlquiler(i) {
@@ -67,7 +65,6 @@ function aceptarAlquiler(i) {
     row.style.backgroundColor = "#86db72";
     myTable.rows[j].cells[7].colSpan = 3;
     myTable.rows[j].cells[7].innerHTML = "Aceptado";
-    //myTable.rows[j].deleteCell(9);
     myTable.rows[j].deleteCell(8);
     alquileresAceptados(i);
 }
@@ -82,7 +79,6 @@ function cargarAceptados() {
             var row = myTable.rows[j];
             row.style.backgroundColor = "#86db72";
             myTable.rows[j].cells[7].innerHTML = "Aceptado";
-            //myTable.rows[j].deleteCell(9);
             myTable.rows[j].deleteCell(8);
             myTable.rows[j].cells[7].colSpan = 3;
         }
@@ -96,7 +92,6 @@ function rechazarAlquiler(i) {
     row.style.backgroundColor = "#FF8066";
     myTable.rows[j].cells[7].colSpan = 3;
     myTable.rows[j].cells[7].innerHTML = "Rechazado";
-    //myTable.rows[j].deleteCell(9);
     myTable.rows[j].deleteCell(8);
     alquileresRechazados(i);
 }
@@ -112,33 +107,12 @@ function cargarRechazados() {
             row.style.backgroundColor = "#FF8066";
             myTable.rows[j].cells[7].colSpan = 3;
             myTable.rows[j].cells[7].innerHTML = "Rechazado";
-            //myTable.rows[j].deleteCell(9);
             myTable.rows[j].deleteCell(8);
             myTable.rows[j].cells[7].colSpan = 3;
         }
     }
 }
 
-/*
-function eliminarAlquiler(i) {
-    var myTable = document.getElementById("tabla");
-    var j = myTable.getElementsByTagName('tr').length - 1 - i;
-    myTable.deleteRow(j);
-    alquileresEliminados(i);
-}
-
-function cargarEliminados() {
-    var eliminados = readArrayFromLocalStorage("AlquileresEliminados");
-    if (eliminados !== null) {
-        var myTable = document.getElementById("tabla");
-        for (var k = 0; k < eliminados.length; k++) {
-            var i = eliminados[k].id;
-            var j = myTable.getElementsByTagName('tr').length - 1 - i;
-            myTable.deleteRow(j);
-        }
-    }
-}
-*/
 function alquileresAceptados(i) {
     var addResultArray = [];
 
@@ -212,41 +186,3 @@ function alquileresRechazados(i) {
     addResultArray1.push(current_add_result)
     localStorage.setItem("AlquileresRechazadosRegistros", JSON.stringify(addResultArray1));
 }
-
-/*
-function alquileresEliminados(i) {
-    var addResultArray = [];
-
-    if (localStorage.getItem("AlquileresEliminados") !== null) {
-        addResultArray = JSON.parse(localStorage.getItem("AlquileresEliminados"));
-    }
-
-    var current_add_result = {
-        id: i
-    }
-
-    addResultArray.push(current_add_result)
-    localStorage.setItem("AlquileresEliminados", JSON.stringify(addResultArray));
-
-    var addResultArray1 = [];
-
-    if (localStorage.getItem("AlquileresEliminadosRegistros") !== null) {
-        addResultArray1 = JSON.parse(localStorage.getItem("AlquileresEliminadosRegistros"));
-    }
-    var array = readArrayFromLocalStorage("lAddResultArray");
-
-    var current_add_result = {
-        user: array[i].user,
-        email: array[i].email,
-        modelo: array[i].modelo,
-        cantidad: array[i].cantidad,
-        ubicacion: array[i].ubicacion,
-        fecha: array[i].fecha,
-        tiempo: array[i].tiempo,
-        estado: "Eliminado"
-    }
-
-    addResultArray1.push(current_add_result)
-    localStorage.setItem("AlquileresEliminadosRegistros", JSON.stringify(addResultArray1));
-}
-*/
--- static/script_admin_cot.js
+++ static/script_admin_cot.js
@@ -48,36 +48,150 @@ function loadAddDataFromAllUsers() {
         row.insertCell(9).innerHTML = addResult.desc;
         row.insertCell(10).innerHTML = "<button onclick='aceptarCotizacion(" + index + ")'>Aceptar</button><input type='hidden' id='" + index + "'>";
         row.insertCell(11).innerHTML = "<button onclick='rechazarCotizacion(" + index + ")'>Rechazar</button><input type='hidden' id='" + index + "'>";
-        row.insertCell(12).innerHTML = "<button onclick='eliminarCotizacion(" + index + ")'>Eliminar</button><input type='hidden' id='" + index + "'>";
         addResultArrayPro[index].estado = "Recibido";
         index++;
         addResultToStorageRegistrosRecibidos(addResult.fec3, addResult.tip, addResult.cli, addResult.nom, addResult.cor, addResult.num, addResult.fec1, addResult.fec2, addResult.ubi, addResult.desc)
 
     }
+    cargarAceptados();
+    cargarRechazados();
 
 }
 
-function deleteElementByIndex(pIndex) {
-    var comprobacion = confirm("Confirmas elminar esta solicitud de cotización?");
-    if (comprobacion == true) {
-        deleteElementFromLocalStorage(pIndex);
-        deleteElementFromTable(pIndex);
+function aceptarCotizacion(i) {
+    var myTable = document.getElementById("userTableClient");
+    var j = myTable.getElementsByTagName('tr').length - 1 - i;
+    var row = myTable.rows[j];
+    row.style.backgroundColor = "#86db72";
+    myTable.rows[j].cells[10].colSpan = 3;
+    myTable.rows[j].cells[10].innerHTML = "Aceptado";
+    myTable.rows[j].deleteCell(11);
+    cotizacionesAceptadas(i);
+}
+
+function cotizacionesAceptadas(i) {
+    var addResultArray = [];
+
+    if (localStorage.getItem("CotizacionesAceptadas") !== null) {
+        addResultArray = JSON.parse(localStorage.getItem("CotizacionesAceptadas"));
+    }
+
+    var current_add_result = {
+        id: i
+    }
+
+    addResultArray.push(current_add_result)
+    localStorage.setItem("CotizacionesAceptadas", JSON.stringify(addResultArray));
+
+
+    var addResultArray1 = [];
+
+    if (localStorage.getItem("CotizacionesAceptadasRegistros") !== null) {
+        addResultArray1 = JSON.parse(localStorage.getItem("CotizacionesAceptadasRegistros"));
+    }
+    var array = readArrayFromLocalStorage("lAddResultArrayPro");
+
+    var current_add_result = {
+        fec3: array[i].fec3,
+        tip: array[i].tip,
+        cli: array[i].cli,
+        nom: array[i].nom,
+        cor: array[i].cor,
+        num: array[i].num,
+        fec1: array[i].fec1,
+        fec2: array[i].fec2,
+        ubi: array[i].ubi,
+        desc: array[i].desc,
+        estado: "Aceptado"
+    }
+
+    addResultArray1.push(current_add_result)
+    localStorage.setItem("CotizacionesAceptadasRegistros", JSON.stringify(addResultArray1));
+
+}
+
+function cargarAceptados() {
+    var aceptados = readArrayFromLocalStorage("CotizacionesAceptadas");
+    if (aceptados !== null) {
+        var myTable = document.getElementById("userTableClient");
+        for (var k = 0; k < aceptados.length; k++) {
+            var i = aceptados[k].id;
+            var j = myTable.getElementsByTagName('tr').length - 1 - i;
+            var row = myTable.rows[j];
+            row.style.backgroundColor = "#86db72";
+            myTable.rows[j].cells[10].colSpan = 3;
+            myTable.rows[j].cells[10].innerHTML = "Aceptado";
+            myTable.rows[j].deleteCell(11);
+        }
     }
 }
 
-function deleteElementFromLocalStorage(pIndex) {
-    var addResultArrayPro = JSON.parse(localStorage.getItem("lAddResultArrayPro"));
-    addResultArrayPro.splice(pIndex, 1);
-    localStorage.setItem("lAddResultArrayPro", JSON.stringify(addResultArrayPro));
+function rechazarCotizacion(i) {
+    var myTable = document.getElementById("userTableClient");
+    var j = myTable.getElementsByTagName('tr').length - 1 - i;
+    var row = myTable.rows[j];
+    row.style.backgroundColor = "#FF8066";
+    myTable.rows[j].cells[10].colSpan = 3;
+    myTable.rows[j].cells[10].innerHTML = "Rechazado";
+    myTable.rows[j].deleteCell(11);
+    cotizacionesRechazadas(i);
 }
 
-function deleteElementFromTable(pIndex) {
-    var element = document.getElementById(pIndex);
-    var parent = getElementParent(element, 3);
-    var child = getElementParent(element, 2);
-    parent.removeChild(child);
+function cotizacionesRechazadas(i) {
+    var addResultArray = [];
+
+    if (localStorage.getItem("CotizacionesRechazadas") !== null) {
+        addResultArray = JSON.parse(localStorage.getItem("CotizacionesRechazadas"));
+    }
+
+    var current_add_result = {
+        id: i
+    }
+
+    addResultArray.push(current_add_result)
+    localStorage.setItem("CotizacionesRechazadas", JSON.stringify(addResultArray));
+
+    var addResultArray1 = [];
+
+    if (localStorage.getItem("CotizacionesRechazadasRegistros") !== null) {
+        addResultArray1 = JSON.parse(localStorage.getItem("CotizacionesRechazadasRegistros"));
+    }
+    var array = readArrayFromLocalStorage("lAddResultArrayPro");
+
+    var current_add_result = {
+        fec3: array[i].fec3,
+        tip: array[i].tip,
+        cli: array[i].cli,
+        nom: array[i].nom,
+        cor: array[i].cor,
+        num: array[i].num,
+        fec1: array[i].fec1,
+        fec2: array[i].fec2,
+        ubi: array[i].ubi,
+        desc: array[i].desc,
+        estado: "Rechazado"
+    }
+
+    addResultArray1.push(current_add_result)
+    localStorage.setItem("CotizacionesRechazadasRegistros", JSON.stringify(addResultArray1));
 }
 
+
+function cargarRechazados() {
+    var rechazados = readArrayFromLocalStorage("CotizacionesRechazadas");
+    if (rechazados !== null) {
+        var myTable = document.getElementById("userTableClient");
+        for (var k = 0; k < rechazados.length; k++) {
+            var i = rechazados[k].id;
+            var j = myTable.getElementsByTagName('tr').length - 1 - i;
+            var row = myTable.rows[j];
+            row.style.backgroundColor = "#FF8066";
+            myTable.rows[j].cells[10].colSpan = 3;
+            myTable.rows[j].cells[10].innerHTML = "Rechazado";
+            myTable.rows[j].deleteCell(11);
+        }
+    }
+}
 
 function readArrayFromSessionStorage(keyName) {
     return JSON.parse(sessionStorage.getItem(keyName))
