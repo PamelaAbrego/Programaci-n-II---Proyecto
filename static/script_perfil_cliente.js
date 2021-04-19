bce83ee1abf0e48c541dfc1ec0cfd1d334826294
Pamela Abrego <pamelabregor@gmail.com>
2021-04-19 01:00:45 -0600

    JS TERMINADO

--- static/script_perfil_cliente.js
+++ static/script_perfil_cliente.js
@@ -43,6 +43,50 @@ function registrosRecibidos() {
     }
 }
 
+function registrosAceptados() {
+    var aceptados = readArrayFromLocalStorage("AlquileresAceptadosRegistros");
+    var myTable = document.getElementById("tabla");
+    var numRows = myTable.getElementsByTagName('tr').length - 1
+    if (aceptados !== null) {
+        for (var i = 1; i <= numRows; i++) {
+            user = myTable.rows.item(i).cells.item(0).innerHTML
+            email = myTable.rows.item(i).cells.item(1).innerHTML
+            modelo = myTable.rows.item(i).cells.item(2).innerHTML
+            cantidad = myTable.rows.item(i).cells.item(3).innerHTML
+            ubicacion = myTable.rows.item(i).cells.item(4).innerHTML
+            fecha = myTable.rows.item(i).cells.item(5).innerHTML
+            tiempo = myTable.rows.item(i).cells.item(6).innerHTML
+            for (var j = 0; j < aceptados.length; j++) {
+                if (user === aceptados[j].user && email === aceptados[j].email && modelo === aceptados[j].modelo && cantidad === String(aceptados[j].cantidad) && ubicacion === aceptados[j].ubicacion && fecha === aceptados[j].fecha && tiempo === String(aceptados[j].tiempo)) {
+                    myTable.rows.item(i).cells.item(7).innerHTML = "Aceptado";
+                }
+            }
+        }
+    }
+}
+
+function registrosRechazados() {
+    var rechazados = readArrayFromLocalStorage("AlquileresRechazadosRegistros");
+    var myTable = document.getElementById("tabla");
+    var numRows = myTable.getElementsByTagName('tr').length - 1
+    if (rechazados !== null) {
+        for (var i = 1; i <= numRows; i++) {
+            user = myTable.rows.item(i).cells.item(0).innerHTML
+            email = myTable.rows.item(i).cells.item(1).innerHTML
+            modelo = myTable.rows.item(i).cells.item(2).innerHTML
+            cantidad = myTable.rows.item(i).cells.item(3).innerHTML
+            ubicacion = myTable.rows.item(i).cells.item(4).innerHTML
+            fecha = myTable.rows.item(i).cells.item(5).innerHTML
+            tiempo = myTable.rows.item(i).cells.item(6).innerHTML
+            for (var j = 0; j < rechazados.length; j++) {
+                if (user === rechazados[j].user && email === rechazados[j].email && modelo === rechazados[j].modelo && cantidad === String(rechazados[j].cantidad) && ubicacion === rechazados[j].ubicacion && fecha === rechazados[j].fecha && tiempo === String(rechazados[j].tiempo)) {
+                    myTable.rows.item(i).cells.item(7).innerHTML = "Rechazado";
+                }
+            }
+        }
+    }
+}
+
 function cargarDatos() {
     var array = readArrayFromLocalStorage("lAddResultArray");
     var myTable = document.getElementById("tabla");
@@ -67,6 +111,8 @@ function cargarDatos() {
         document.getElementById("result_alquileres").innerHTML = "No se han realizado peticiones de alquiler."
     }
     registrosRecibidos();
+    registrosAceptados();
+    registrosRechazados();
 }
 
 function cargarDatosTablaProy() {
@@ -94,6 +140,8 @@ function cargarDatosTablaProy() {
         document.getElementById("result_cotizaciones").innerHTML = "No se han realizado peticiones de cotizaciones."
     }
     registrosRecibidosCot();
+    registrosAceptadosCot();
+    registrosRechazadosCot();
 }
 
 
@@ -114,6 +162,52 @@ function registrosRecibidosCot() {
             for (var j = 0; j < recibidos.length; j++) {
                 if (fec3 === recibidos[j].fec3 && tip === recibidos[j].tip && nom === recibidos[j].nom && cor === recibidos[j].cor && num === recibidos[j].num && fec1 === recibidos[j].fec1 && fec2 === recibidos[j].fec2 && ubi === recibidos[j].ubi) {
                     myTable.rows.item(i).cells.item(8).innerHTML = "Recibido";
+                }
+            }
+        }
+    }
+}
+
+function registrosAceptadosCot() {
+    var aceptados = readArrayFromLocalStorage("CotizacionesAceptadasRegistros");
+    var myTable = document.getElementById("tablaProy");
+    var numRows = myTable.getElementsByTagName('tr').length - 1
+    if (aceptados !== null) {
+        for (var i = 1; i <= numRows; i++) {
+            fec3 = myTable.rows.item(i).cells.item(0).innerHTML
+            tip = myTable.rows.item(i).cells.item(1).innerHTML
+            nom = myTable.rows.item(i).cells.item(2).innerHTML
+            cor = myTable.rows.item(i).cells.item(3).innerHTML
+            num = myTable.rows.item(i).cells.item(4).innerHTML
+            fec1 = myTable.rows.item(i).cells.item(5).innerHTML
+            fec2 = myTable.rows.item(i).cells.item(6).innerHTML
+            ubi = myTable.rows.item(i).cells.item(7).innerHTML
+            for (var j = 0; j < aceptados.length; j++) {
+                if (fec3 === aceptados[j].fec3 && tip === aceptados[j].tip && nom === aceptados[j].nom && cor === aceptados[j].cor && num === aceptados[j].num && fec1 === aceptados[j].fec1 && fec2 === aceptados[j].fec2 && ubi === aceptados[j].ubi) {
+                    myTable.rows.item(i).cells.item(8).innerHTML = "Aceptado";
+                }
+            }
+        }
+    }
+}
+
+function registrosRechazadosCot() {
+    var rechazados = readArrayFromLocalStorage("CotizacionesRechazadasRegistros");
+    var myTable = document.getElementById("tablaProy");
+    var numRows = myTable.getElementsByTagName('tr').length - 1
+    if (rechazados !== null) {
+        for (var i = 1; i <= numRows; i++) {
+            fec3 = myTable.rows.item(i).cells.item(0).innerHTML
+            tip = myTable.rows.item(i).cells.item(1).innerHTML
+            nom = myTable.rows.item(i).cells.item(2).innerHTML
+            cor = myTable.rows.item(i).cells.item(3).innerHTML
+            num = myTable.rows.item(i).cells.item(4).innerHTML
+            fec1 = myTable.rows.item(i).cells.item(5).innerHTML
+            fec2 = myTable.rows.item(i).cells.item(6).innerHTML
+            ubi = myTable.rows.item(i).cells.item(7).innerHTML
+            for (var j = 0; j < rechazados.length; j++) {
+                if (fec3 === rechazados[j].fec3 && tip === rechazados[j].tip && nom === rechazados[j].nom && cor === rechazados[j].cor && num === rechazados[j].num && fec1 === rechazados[j].fec1 && fec2 === rechazados[j].fec2 && ubi === rechazados[j].ubi) {
+                    myTable.rows.item(i).cells.item(8).innerHTML = "Rechazado";
                 }
             }
         }
